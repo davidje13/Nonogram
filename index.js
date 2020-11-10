ff84defa73a914b7425f74c01b34446c3cf9b0a9
David Evans <davidje13@users.noreply.github.com>
2020-11-10 20:21:22 +0000

    Add CLI support for solving multiple files in one execution

--- index.js
+++ index.js
@@ -21,32 +21,43 @@ const solver = new Solver([
   solverPerlRegexp,
 ]);
 
-const gameFile = process.argv[2] || process.stdin.fd;
-const game = compileGame(JSON.parse(fs.readFileSync(gameFile)));
-const state = makeState(game);
+function run(gameFile) {
+  const game = compileGame(JSON.parse(fs.readFileSync(gameFile)));
+  const state = makeState(game);
 
-const tmBegin = performance.now();
-let tmEnd;
-try {
-  solver.solve(game.rules, state);
-  tmEnd = performance.now();
-  drawGameState(game, state);
-} catch (e) {
-  tmEnd = performance.now();
-  if (e instanceof AmbiguousError) {
-    process.stderr.write('Multiple solutions found. Examples (there may be more):\n\n');
-    for (const exampleState of e.exampleStates) {
-      drawGameState(game, exampleState);
-      process.stderr.write('\n');
-    }
-    process.stderr.write('Solution before uncertainty:\n\n');
+  const tmBegin = performance.now();
+  let tmEnd;
+  try {
+    solver.solve(game.rules, state);
+    tmEnd = performance.now();
     drawGameState(game, state);
-  } else {
-    process.stderr.write(`${e}\n`);
+  } catch (e) {
+    tmEnd = performance.now();
+    if (e instanceof AmbiguousError) {
+      process.stderr.write('Multiple solutions found. Examples (there may be more):\n\n');
+      for (const exampleState of e.exampleStates) {
+        drawGameState(game, exampleState);
+        process.stderr.write('\n');
+      }
+      process.stderr.write('Solution before uncertainty:\n\n');
+      drawGameState(game, state);
+    } else {
+      process.stderr.write(`${e}\n`);
+    }
   }
+
+  process.stdout.write(`\n`);
+  process.stdout.write(`Short image: ${toShortByImage(game, state)}\n`);
+  process.stdout.write(`Short rules: ${toShortByRules(game)}\n`);
+  process.stdout.write(`Solving time: ${(tmEnd - tmBegin).toFixed(1)}ms\n`);
 }
 
-process.stdout.write(`\n`);
-process.stdout.write(`Short image: ${toShortByImage(game, state)}\n`);
-process.stdout.write(`Short rules: ${toShortByRules(game)}\n`);
-process.stdout.write(`Solving time: ${(tmEnd - tmBegin).toFixed(1)}ms\n`);
+if (process.argv.length < 3) {
+  run(process.stdin.fd);
+} else {
+  for (let i = 2; i < process.argv.length; ++ i) {
+    const gameFile = process.argv[i];
+    process.stdout.write(`\nSolving ${gameFile}\n`);
+    run(gameFile);
+  }
+}
