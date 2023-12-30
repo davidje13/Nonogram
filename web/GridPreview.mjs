a74a2b8cf3cc08a58f8024a136913eba7ddf8e4e
David Evans <davidje13@users.noreply.github.com>
2023-12-30 02:49:01 +0000

    Add crude UI for listing games

--- web/GridPreview.mjs
+++ web/GridPreview.mjs
@@ -1,5 +1,6 @@
 export class GridPreview {
   constructor(rules = { rows: [[]], cols: [[]] }) {
+    this.enhancementSteps = 2;
     this.canvas = document.createElement('canvas');
     this.canvas.className = 'grid-preview';
     this.ctx = this.canvas.getContext('2d', { alpha: true, willReadFrequently: true });
@@ -12,14 +13,60 @@ export class GridPreview {
     this.canvas.width = w;
     this.canvas.height = h;
 
-    const xDensity = rules.cols.map(getDensity(h));
-    const yDensity = rules.rows.map(getDensity(w));
+    let nx = w;
+    let ny = h;
+    const xSums = rules.cols.map(sum);
+    const ySums = rules.rows.map(sum);
 
+    const ps = [];
+    for (let i = 0; i < w * h; ++i) {
+      ps[i] = null;
+    }
+    for (let step = 0; step < this.enhancementSteps; ++step) {
+      let change = false;
+      for (let y = 0; y < h; ++y) {
+        const d = ySums[y];
+        if (d === 0 || d === nx) {
+          const v = d > 0 ? 1 : 0;
+          for (let x = 0; x < w; ++x) {
+            if (xSums[x] !== null) {
+              ps[y * w + x] = v;
+              xSums[x] -= v;
+            }
+          }
+          ySums[y] = null;
+          --ny;
+          change = true;
+        }
+      }
+      for (let x = 0; x < w; ++x) {
+        const d = xSums[x];
+        if (d === 0 || d === ny) {
+          const v = d > 0 ? 1 : 0;
+          for (let y = 0; y < h; ++y) {
+            if (ySums[y] !== null) {
+              ps[y * w + x] = v;
+              ySums[y] -= v;
+            }
+          }
+          xSums[x] = null;
+          --nx;
+          change = true;
+        }
+      }
+      if (!change) {
+        break;
+      }
+    }
+    const mx = 1 / nx;
+    const my = 1 / ny;
     const dat = this.ctx.createImageData(w, h);
     for (let y = 0; y < h; ++y) {
+      const sy = (ySums[y] ?? 0.5) * mx;
       for (let x = 0; x < w; ++x) {
-        const p = combineP(xDensity[x], yDensity[y]);
-        dat.data[(y * w + x) * 4 + 3] = (p * 255 + 0.5)|0;
+        const p = y * w + x;
+        const d = ps[p] ?? combineP((xSums[x] ?? 0.5) * my, sy);
+        dat.data[p * 4 + 3] = (d * 255 + 0.5)|0;
       }
     }
     this.ctx.putImageData(dat, 0, 0);
@@ -35,4 +82,4 @@ function combineP(p1, p2) {
   return (p1 * p2) / (1 - p1 - p2 + 2 * p1 * p2);
 }
 
-const getDensity = (size) => (rule) => rule ? rule.reduce((a, b) => a + b, 0) / size : 0.5;
+const sum = (rule) => rule?.reduce((a, b) => a + b, 0) ?? null;
--- web/index.mjs
+++ web/index.mjs
@@ -1,5 +1,5 @@
 import { UNKNOWN, OFF, ON } from '../src/constants.mjs';
-import { compileGame, rulesForImage } from '../src/game.mjs';
+import { compileGame, rulesForImage, extractRules } from '../src/game.mjs';
 import { AmbiguousError } from '../src/solver/errors.mjs';
 import { perlRegexp } from '../src/solver/methods/isolated-rules/perl-regexp.mjs';
 import { LiveSolver } from '../src/solver/LiveSolver.mjs';
@@ -11,13 +11,23 @@ import { GamePlayer } from './GamePlayer.mjs';
 const root = document.createElement('div');
 document.body.append(root);
 
+const player = new GamePlayer({ cellSize: 23, border: 1, ruleChecker: perlRegexp });
+
+const playerTitle = document.createElement('h2');
+playerTitle.textContent = '';
+
 const info = document.createElement('div');
 const definition = document.createElement('pre');
 definition.className = 'definition';
 
 const liveSolver = new LiveSolver();
 
-const editor = new GridView({
+const editor = document.createElement('div');
+editor.className = 'editor';
+const editorTitle = document.createElement('h2');
+editorTitle.textContent = 'Editor';
+
+const editorView = new GridView({
   width: 5,
   height: 5,
   cellWidth: 23,
@@ -28,7 +38,7 @@ const editor = new GridView({
 
 liveSolver.addEventListener('begin', () => {
   info.textContent = 'Checking game\u2026';
-  editor.clearMarked();
+  editorView.clearMarked();
 });
 
 liveSolver.addEventListener('complete', ({ detail }) => {
@@ -37,7 +47,7 @@ liveSolver.addEventListener('complete', ({ detail }) => {
   } else if (detail.error instanceof AmbiguousError) {
     for (let i = 0; i < detail.board.length; ++i) {
       if (detail.board[i] === UNKNOWN) {
-        editor.setMarked(i, 0, true);
+        editorView.setMarked(i, 0, true);
       }
     }
     info.textContent = 'Game is ambiguous.';
@@ -50,14 +60,13 @@ liveSolver.addEventListener('complete', ({ detail }) => {
 const preview = new GridPreview();
 
 const editorChanged = () => {
-  const rules = rulesForImage(editor.getGrid());
+  const rules = rulesForImage(editorView.getGrid());
   preview.setRules(rules);
-  player.setRules(rules);
   definition.textContent = JSON.stringify(rules);
   liveSolver.solveInBackground(compileGame(rules));
 };
 
-editor.addEventListener('change', editorChanged);
+editorView.addEventListener('change', editorChanged);
 
 function makeButton(text, fn) {
   const btn = document.createElement('button');
@@ -70,18 +79,24 @@ function makeButton(text, fn) {
 const options = document.createElement('div');
 options.className = 'options';
 options.append(
-  makeButton('clear', () => editor.fill(OFF)),
+  makeButton('clear', () => editorView.fill(OFF)),
+  makeButton('play', () => {
+    const rules = rulesForImage(editorView.getGrid());
+    playerTitle.textContent = 'New game';
+    player.setRules(rules);
+    player.clear();
+  }),
 );
 
-const editorResizer = new Resizer(editor.canvas, {
-  getCurrentSize: () => editor.getGrid(),
-  xScale: editor.getTotalCellSize().width,
-  yScale: editor.getTotalCellSize().height,
+const editorResizer = new Resizer(editorView.canvas, {
+  getCurrentSize: () => editorView.getGrid(),
+  xScale: editorView.getTotalCellSize().width,
+  yScale: editorView.getTotalCellSize().height,
   xMin: 1,
   yMin: 1,
 });
 
-editorResizer.addEventListener('change', ({ detail }) => editor.resize({
+editorResizer.addEventListener('change', ({ detail }) => editorView.resize({
   width: detail.width,
   height: detail.height,
   dx: detail.dx,
@@ -89,7 +104,49 @@ editorResizer.addEventListener('change', ({ detail }) => editor.resize({
   fill: OFF,
 }));
 
-const player = new GamePlayer({ cellSize: 23, border: 1, ruleChecker: perlRegexp });
-root.append(options, editorResizer.container, info, preview.canvas, player.container, definition);
+editor.append(editorTitle, options, editorResizer.container, info, preview.canvas, definition);
+
+const gameList = document.createElement('div');
+gameList.className = 'game-list';
+
+function addGame(rules, name) {
+  const row = document.createElement('a');
+  row.className = 'row';
+  row.setAttribute('href', '#');
+  const label = document.createElement('div');
+  label.className = 'label';
+  label.textContent = name;
+  const preview = new GridPreview(rules);
+  row.append(preview.canvas, label);
+  row.addEventListener('click', (e) => {
+    e.preventDefault();
+    playerTitle.textContent = name;
+    player.setRules(rules);
+    player.clear();
+  });
+  gameList.append(row);
+}
+
+window.addEventListener('dragover', (e) => {
+  e.preventDefault();
+});
+
+window.addEventListener('drop', async (e) => {
+  e.preventDefault();
+  const files = [...e.dataTransfer.items]
+    .filter((item) => item.kind === 'file')
+    .map((item) => item.getAsFile());
+
+  for (const file of files) {
+    try {
+      const definition = JSON.parse(await file.text());
+      addGame(extractRules(definition), definition.name || definition.description || file.name);
+    } catch (e) {
+      console.error(e);
+    }
+  }
+});
+
+root.append(editor, playerTitle, player.container, gameList);
 
 editorChanged();
